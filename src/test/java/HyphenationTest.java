376a50a001a8d03c08fc375d27b16f45285de556
Bert Frees <bertfrees@gmail.com>
2015-02-27 13:43:21 +0100

    Test with both libhyphen and texhyph

---
+++ src/test/java/HyphenationTest.java
@@ -0,0 +1,68 @@
+import javax.inject.Inject;
+
+import org.daisy.pipeline.braille.libhyphen.Libhyphen;
+import org.daisy.pipeline.braille.tex.TexHyphenator;
+import static org.daisy.pipeline.braille.Utilities.URIs.asURI;
+
+import static org.daisy.pipeline.pax.exam.Options.brailleModule;
+import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
+import static org.daisy.pipeline.pax.exam.Options.forThisPlatform;
+import static org.daisy.pipeline.pax.exam.Options.logbackBundles;
+import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
+import static org.daisy.pipeline.pax.exam.Options.thisBundle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.util.PathUtils;
+
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class HyphenationTest {
+	
+	@Inject
+	Libhyphen libhyphen;
+	
+	@Test
+	public void testWithLibhyphen() {
+		assertEquals("au\u00ADto", libhyphen.hyphenate(asURI("hyph-fi.dic"), "auto"));
+	}
+	
+	@Inject
+	TexHyphenator texhyph;
+	
+	@Test
+	public void testWithTexhyph() {
+		assertEquals("au\u00ADto", texhyph.hyphenate(asURI("hyph-fi.tex"), "auto"));
+	}
+	
+	@Configuration
+	public Option[] config() {
+		return options(
+			logbackConfigFile(),
+			logbackBundles(),
+			felixDeclarativeServices(),
+			mavenBundle().groupId("com.google.guava").artifactId("guava").versionAsInProject(),
+			mavenBundle().groupId("net.java.dev.jna").artifactId("jna").versionAsInProject(),
+			mavenBundle().groupId("org.daisy.bindings").artifactId("jhyphen").versionAsInProject(),
+			mavenBundle().groupId("com.googlecode.texhyphj").artifactId("texhyphj").versionAsInProject(),
+			brailleModule("common-java"),
+			brailleModule("libhyphen-core"),
+			forThisPlatform(brailleModule("libhyphen-native")),
+			brailleModule("texhyph-core"),
+			thisBundle(true),
+			junitBundles()
+		);
+	}
+}
