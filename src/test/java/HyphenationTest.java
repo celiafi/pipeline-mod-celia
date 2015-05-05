37d8e348dec7f8320caee07e0e6c7ff1e9857f3a
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2015-05-05 10:39:22 +0300

    add consonant clusters and exceptional vowel cases

--- src/test/java/HyphenationTest.java
+++ src/test/java/HyphenationTest.java
@@ -52,11 +52,12 @@ public class HyphenationTest {
 		testCases.add(new String[]{"Simple hyphenation", "au\u00ADto", "auto"});
 		testCases.add(new String[]{"Diphthong", "näin", "näin"});
 		testCases.add(new String[]{"Dual vowel", "nä\u00ADen", "näen"});
+		testCases.add(new String[]{"Longer vowel strings", "ai\u00ADe tau\u00ADois\u00ADsa", "aie tauoissa"});
 		testCases.add(new String[]{"Word-initial single vowels", "alas eteen iso oli uros yhä äly öky", "alas eteen iso oli uros yhä äly öky"});
-		testCases.add(new String[]{"Vowel triplets and compound words", "ka\u00ADvi\u00ADaa\u00ADri ra\u00ADdi\u00ADo\u00ADuu\u00ADti\u00ADset vir\u00ADhe\u00ADuu\u00ADti\u00ADnen maa\u00ADot\u00ADte\u00ADlu ve\u00ADto\u00ADuis\u00ADtin sää\u00ADil\u00ADmi\u00ADö suo\u00ADa\u00ADlu\u00ADe uraa\u00ADuur\u00ADta\u00ADva puu\u00ADis\u00ADtu\u00ADtus ka\u00ADla\u00ADuu\u00ADni ki\u00ADvi\u00ADuu\u00ADni va\u00ADle\u00ADaa\u00ADri\u00ADa pii\u00ADotus", "kaviaari radiouutiset virheuutinen maaottelu vetouistin sääilmiö suoalue uraauurtava puuistutus kalauuni kiviuuni valeaaria piiotus"});
+		testCases.add(new String[]{"Vowel triplets and compound words", "ka\u00ADvi\u00ADaa\u00ADri ra\u00ADdi\u00ADo\u00ADuu\u00ADti\u00ADset vir\u00ADhe\u00ADuu\u00ADti\u00ADnen maa\u00ADot\u00ADte\u00ADlu ve\u00ADto\u00ADuis\u00ADtin sää\u00ADil\u00ADmi\u00ADö suo\u00ADa\u00ADlu\u00ADe uraa\u00ADuur\u00ADta\u00ADva puu\u00ADis\u00ADtu\u00ADtus ka\u00ADla\u00ADuu\u00ADni ki\u00ADvi\u00ADuu\u00ADni va\u00ADle\u00ADaa\u00ADri\u00ADa pii\u00ADotus maa\u00ADui\u00ADma\u00ADla", "kaviaari radiouutiset virheuutinen maaottelu vetouistin sääilmiö suoalue uraauurtava puuistutus kalauuni kiviuuni valeaaria piiotus maauimala"});
 		testCases.add(new String[]{"Compound word with hyphen", "rek\u00ADka-\u200Bau\u00ADto", "rekka-auto"});
 		testCases.add(new String[]{"Native consonant clusters", "kars\u00ADta\u00ADvers\u00ADtas", "karstaverstas"});
-		testCases.add(new String[]{"Borrowed consonant clusters", "eks\u00ADpres\u00ADsii\u00ADvi\u00ADnen verk\u00ADko\u00ADstra\u00ADte\u00ADgi\u00ADa", "ekspressiivinen verkkostrategia"});
+		testCases.add(new String[]{"Borrowed consonant clusters", "eks\u00ADpres\u00ADsii\u00ADvi\u00ADnen verk\u00ADko\u00ADstra\u00ADte\u00ADgi\u00ADa yö\u00ADklu\u00ADbil\u00ADle ar\u00ADtik\u00ADla klo\u00ADset\u00ADti klii\u00ADmak\u00ADsi ak\u00ADkre\u00ADdi\u00ADtoin\u00ADti ag\u00ADgres\u00ADsi\u00ADo", "ekspressiivinen verkkostrategia yöklubille artikla klosetti kliimaksi akkreditointi aggressio"});
 		testCases.add(new String[]{"Non-word-ending and word-ending s", "os\u00ADtos", "ostos"});
 	}
 	
